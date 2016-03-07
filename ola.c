1b61dc5815a1b377f5be897a0a2b317c57d5281c
Rogério Pereira <rogerio@groupsofter.com.br>
2016-03-07 19:52:30 -0300

    correção de espaçamento no arquivo

--- ola.c
+++ ola.c
@@ -4,10 +4,10 @@
 int main(int argc, char *argv[])
 {
 	char argumento[30];
-	
+
 	/* o valor padrão é Mundo */
 	strcpy(argumento, argc > 2 ? argv[1]:"Mundo");
 	printf("ola, %s !", argumento);
-	
+
 	return 0;
 }
